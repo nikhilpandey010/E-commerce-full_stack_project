f7cbe990b086dd30598aad8bbfe9dadfab8cb5a3
Your Name <you@example.com>
2025-12-28 12:18:49 +0530

    add frontend url

--- Django/api/api/settings.py
+++ Django/api/api/settings.py
@@ -158,12 +158,12 @@ REST_FRAMEWORK = {
     # ]
 }
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://e-commerce-full-stack-project-eight.vercel.app"
+CORS_ALLOWED_ORIGINS = [
+    "https://e-commerce-full-stack-project-eight.vercel.app"
    
-# ]
+]
 
-CORS_ALLOW_ALL_ORIGINS = True
+
 
 
 CORS_ALLOW_METHODS = (
