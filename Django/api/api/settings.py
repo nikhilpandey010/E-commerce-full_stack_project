91970da7b4450c6260dfb4175ee1faac13c71871
Your Name <you@example.com>
2025-12-28 15:35:43 +0530

    add backend url

--- Django/api/api/settings.py
+++ Django/api/api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = ["https://e-commerce-full-stack-project-2.onrender.com"]
+ALLOWED_HOSTS = ["e-commerce-full-stack-project-2.onrender.com"]
 
 
 
